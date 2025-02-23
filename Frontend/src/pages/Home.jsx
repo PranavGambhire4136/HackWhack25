e5a0ac16b9324380b688fbfccf00e018e1839dd8
aniketramteke06 <aniketramteke.190@gmail.com>
2025-02-24 00:58:32 +0530

    done Hero section

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import heroig from "../Assets/heroimg.jpg";
+import heroig from "../Assets/herologo.png";
 
 function Home() {
   return (
     <div className="w-full flex items-center justify-center px-4 mb-8">
       <section className="max-w-6xl w-full flex flex-col md:flex-row items-center gap-8">
         {/* Left Content */}
-        <div className="pt-50 md:w-1/2 text-center md:text-left pb-2">
+        <div className="pt-16 md:w-1/2 text-center md:text-left pb-2">
           <div className="flex flex-row pb-6">
             <p className="text-black font-semibold text-5xl">Welcome to</p>
             <p className="text-[#191C2D] font-semibold text-5xl">&nbsp;prepMate</p>
@@ -15,23 +15,25 @@ function Home() {
           </div>
 
           <h1 className="text-gray-600 indent-8 text-lg leading-tight">
-          Revolutionize Your Exam Preparation with AI-Powered Personalized Learning. 
+            Revolutionize Your Exam Preparation with AI-Powered Personalized Learning. 
             Get tailored study plans, real-time feedback, and expert guidance to ace your competitive exams.
-
           </h1>
 
-          {/* Buttons */}
-          <button className="border-1 mt-6 px-6 py-3  text-[#1B5465] font-semibold text-lg rounded-full shadow-md hover:bg-#011fc5] transition">
-            <a href="#">Get Started</a>
-          </button>
+          {/* Button */}
+          <a href="#">
+            <button className="border mt-6 px-6 py-3 text-[#1B5465] font-semibold text-lg rounded-full shadow-md 
+              hover:bg-[#011fc5] hover:text-white transition">
+              Get Started
+            </button>
+          </a>
         </div>
 
         {/* Right Content (Image) */}
-        <div className=" pt-70 md:w-1/2 flex justify-center mt-10 md:mt-0">
+        <div className="pt-30 md:w-1/2 flex justify-center mt-10 md:mt-0">
           <img 
-            src="https://source.unsplash.com/500x500/?technology,ai" 
+            src={heroig} 
             alt="AI Buddy" 
-            className="rounded-lg shadow-lg"
+            className="rounded-lg"
           />
         </div>
       </section>
